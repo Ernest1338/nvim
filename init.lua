bc0754dabf789b72279dbae280b6eb6af45423b2
Ernest Gupik <ernestgupik@wp.pl>
2024-06-10 15:41:46 +0200

    Disable some providers at startup

--- init.lua
+++ init.lua
@@ -26,7 +26,11 @@ local builtin_plugs = {
     "shada_plugin",
     "2html_plugin",
     "tutor_mode_plugin",
-    "remote_plugins"
+    "remote_plugins",
+    "python3_provider",
+    "perl_provider",
+    "node_provider",
+    "ruby_provider"
 }
 for i = 1, #builtin_plugs do
     vim.g["loaded_" .. builtin_plugs[i]] = true
