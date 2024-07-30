1cca0917aaff85591b4ef5dfcfc632079c71f087
Ernest Gupik <ernestgupik@wp.pl>
2024-07-31 00:38:01 +0200

    termplug: plain tmux

--- lua/plugins/termplug.lua
+++ lua/plugins/termplug.lua
@@ -4,5 +4,5 @@ local map = vim.keymap.set
 later(function()
     add("Ernest1338/termplug.nvim")
     require("termplug").setup()
-    map({ "n", "t" }, "<A-i>", "<cmd> Term (tmux new -d -s nvim && tmux a -t nvim) || (tmux a -t nvim \\; new-window -c '~') <CR>")
+    map({ "n", "t" }, "<A-i>", "<cmd> Term tmux <CR>")
 end)
