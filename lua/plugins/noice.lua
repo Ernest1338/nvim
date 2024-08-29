38c8f0a6f592f7e7606b48ba5c8c4cdbed59c266
Ernest Gupik <ernestgupik@wp.pl>
2024-08-29 17:12:05 +0200

    Noice: disable lazyredraw

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,5 +2,6 @@ local add, later = MiniDeps.add, MiniDeps.later
 
 later(function()
     add({ source = "folke/noice.nvim", depends = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" } })
+    vim.o.lazyredraw = false
     require("noice").setup()
 end)
