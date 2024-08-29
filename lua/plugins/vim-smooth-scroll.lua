f3312659499dc3e1975728964c2a161d68c64830
Ernest Gupik <ernestgupik@wp.pl>
2024-08-29 17:18:10 +0200

    vim-smooth-scroll: remove conflicting mappings

--- lua/plugins/vim-smooth-scroll.lua
+++ lua/plugins/vim-smooth-scroll.lua
@@ -5,7 +5,5 @@ later(function()
     vim.cmd([[
         noremap <silent> <c-u> :call smooth_scroll#up(&scroll, 0, 2)<CR>zz
         noremap <silent> <c-d> :call smooth_scroll#down(&scroll, 0, 2)<CR>zz
-        noremap <silent> <c-b> :call smooth_scroll#up(&scroll*2, 0, 4)<CR>zz
-        noremap <silent> <c-f> :call smooth_scroll#down(&scroll*2, 0, 4)<CR>zz
     ]])
 end)
