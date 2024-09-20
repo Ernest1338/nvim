1c0f2370525e51d54d32b3cfd624212864cb7858
Ernest Gupik <ernestgupik@wp.pl>
2024-09-20 15:42:33 +0200

    Improve myaiplug

--- lua/plugins/basics/myaiplug.lua
+++ lua/plugins/basics/myaiplug.lua
@@ -1,4 +1,4 @@
-local ask_ai = function(prompt, context, only_code)
+local old_ask_ai = function(prompt, context, only_code)
     local buf = vim.api.nvim_create_buf(true, true)
     vim.api.nvim_set_option_value('filetype', 'markdown', { buf = buf })
     vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "Thinking..." })
@@ -23,6 +23,29 @@ local ask_ai = function(prompt, context, only_code)
             vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
         end)
     end)
+end
+
+local bufnr = 0
+
+local ask_ai = function(prompt, context, only_code)
+    if prompt == "" then return end
+    if context == nil then context = "" end
+    local additional = ''
+    if only_code == true then additional = '-c' end
+
+    local provider = "duckduckgo" -- or phind
+
+    local buf = vim.api.nvim_create_buf(true, true)
+    vim.api.nvim_set_option_value('filetype', 'markdown', { buf = buf })
+    vim.api.nvim_set_current_buf(buf)
+
+    local cmd = "cat << EOF | tgpt --disable-input-limit --provider " ..
+        provider .. " " .. additional .. " \'" .. prompt .. "\' \n " .. context .. "\nEOF\n"
+    print(cmd)
+    vim.fn.termopen(cmd)
+
+    bufnr = bufnr + 1
+    vim.api.nvim_buf_set_name(buf, "AI answer " .. bufnr)
 end
 
 local get_current_selection = function()
@@ -58,13 +81,13 @@ local map = vim.keymap.set
 
 -- Ask AI
 map("n", "<leader>aq", function()
-    local prompt = vim.fn.input("Query: ")
+    local prompt = vim.fn.input("[Ask AI]: ")
     ask_ai(prompt)
 end)
 
 -- Ask AI with current buffer or selection context
 map({ "n", "v" }, "<leader>ai", function()
-    local prompt = vim.fn.input("Query: ")
+    local prompt = vim.fn.input("[AI prompt]: ")
     local context = get_cur_sel_or_buf()
     ask_ai(prompt, context)
 end)
@@ -77,11 +100,11 @@ map("v", "<leader>ad", function()
 end)
 
 -- AI write code
-map("n", "<leader>ac", function()
-    local prompt = "Implement the code which I indicated using text 'HERE'"
-    local context = get_lines_cur_buf()
-    local only_code = true
-    ask_ai(prompt, context, only_code)
+map({ "n", "v" }, "<leader>ac", function()
+    local prompt =
+    'Implement the code which I indicated using text "HERE". Dont include all of the code, just show me the code you written.'
+    local context = get_cur_sel_or_buf()
+    ask_ai(prompt, context, true)
 end)
 
 -- AI improve code
