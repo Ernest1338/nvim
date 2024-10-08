8802df817ea495619e8c440182d17f6e991628ab
Ernest Gupik <ernestgupik@wp.pl>
2024-10-08 23:14:01 +0200

    Termplug: use bash

--- lua/plugins/basics/termplug.lua
+++ lua/plugins/basics/termplug.lua
@@ -3,7 +3,7 @@ local map = vim.keymap.set
 
 later(function()
     add("Ernest1338/termplug.nvim")
-    require("termplug").setup({ shell = "fish" })
+    require("termplug").setup()
     map({ "n", "t" }, "<A-i>", "<cmd> Term <CR>")
     map("n", "<leader>gg", "<cmd> Term lazygit <CR>")
     map({ "n", "t" }, "<C-g>", "<cmd> Term lazygit <CR>")
