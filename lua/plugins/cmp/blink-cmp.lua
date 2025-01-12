0452f67da0978e8a4664da47139553824e522929
Ernest Gupik <ernestgupik@wp.pl>
2025-01-12 17:29:43 +0100

    blink-cmp: update and fix mappings

--- lua/plugins/cmp/blink-cmp.lua
+++ lua/plugins/cmp/blink-cmp.lua
@@ -1,37 +1,29 @@
 local add, later = MiniDeps.add, MiniDeps.later
 
--- NOTE: disable mini.icons lsp kind prepend for this plugin to work
-
 later(function()
     add({
         source = "Saghen/blink.cmp",
-        checkout = "v0.7.6",
+        checkout = "v0.9.2",
         -- Optional
         depends = { "rafamadriz/friendly-snippets" }
     })
 
     require("blink.cmp").setup({
         keymap = {
-            select_prev = { '<S-Tab>', '<Up>', '<C-k>' },
-            select_next = { '<Tab>', '<Down>', '<C-j>' },
-            accept = { '<Enter>' },
-            scroll_documentation_up = { '<C-u>' },
-            scroll_documentation_down = { '<C-d>' },
+            preset = 'enter',
+            ['<S-Tab>'] = { 'select_prev' },
+            ['<Tab>'] = { 'select_next' },
+            ['<Up>'] = { 'select_prev' },
+            ['<Down>'] = { 'select_next' },
+            ['<C-k>'] = { 'select_prev' },
+            ['<C-j>'] = { 'select_next' },
+            ['<C-u>'] = { 'scroll_documentation_up' },
+            ['<C-d>'] = { 'scroll_documentation_down' },
         },
-        trigger = {
-            signature_help = {
-                enabled = true
-            }
+        signature = { enabled = true },
+        completion = {
+            menu = { border = 'rounded' },
+            documentation = { window = { border = 'rounded' } }
         },
-        windows = {
-            autocomplete = {
-                border = 'rounded',
-                draw = 'reversed',
-
-            },
-            documentation = {
-                border = 'rounded',
-            }
-        }
     })
 end)
