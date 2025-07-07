0e398ea1d9a681e11f5c1416a36396269fbee1a4
Ernest Gupik <ernestgupik@wp.pl>
2025-07-07 18:05:19 +0200

    Update copilot config

--- lua/plugins/ai/copilot.lua
+++ lua/plugins/ai/copilot.lua
@@ -1,7 +1,12 @@
 local add, later = MiniDeps.add, MiniDeps.later
 
 later(function()
+    -- Vimscript version
     add("github/copilot.vim")
     vim.cmd("Copilot restart")
     -- NOTE: run `:Copilot setup` before using
+
+    -- Pure lua replacement
+    -- add("zbirenbaum/copilot.lua")
+    -- require("copilot").setup()
 end)
